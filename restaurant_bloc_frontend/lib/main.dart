0498ec4a66de974894d5d87c8093fcd3f0983e75
Makfront0123 <ing.sistemas2019armando@gmail.com>
2025-04-12 19:56:37 -0500

    add comment uni link

--- restaurant_bloc_frontend/lib/main.dart
+++ restaurant_bloc_frontend/lib/main.dart
@@ -28,3 +28,66 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:uni_links/uni_links.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamSubscription? _sub;
+
+  @override
+  void initState() {
+    super.initState();
+    _handleDeepLinks();
+  }
+
+  void _handleDeepLinks() {
+    _sub = uriLinkStream.listen((Uri? uri) {
+      if (uri != null &&
+          uri.scheme == 'restaurantapp' &&
+          uri.host == 'reset-password') {
+        final token = uri.queryParameters['token'];
+        final email = uri.queryParameters['email'];
+
+        // Redirigir al usuario a la pantalla de reset
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => ResetPasswordScreen(email: email!, token: token!),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Bienvenido')),
+      ),
+    );
+  }
+}
+
+ */

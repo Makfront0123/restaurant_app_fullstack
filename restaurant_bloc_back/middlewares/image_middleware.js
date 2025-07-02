491a075d09d5adcb0519494431daf3c81e2c73e0
Armando Sanchez <ing.sistemas2019armando@gmail.com>
2025-07-02 10:56:32 -0500

    Update image_middleware.js

--- restaurant_bloc_back/middlewares/image_middleware.js
+++ restaurant_bloc_back/middlewares/image_middleware.js
@@ -2,13 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Obtener __dirname en ESModules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads/')); // crea esa carpeta si no existe
+    cb(null, path.join(__dirname, '../uploads/')); 
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);

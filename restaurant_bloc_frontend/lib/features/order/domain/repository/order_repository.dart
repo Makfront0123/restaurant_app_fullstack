b69ce1fe12898854e0229666eb3de90f7a38c34e
Armando Sanchez <ing.sistemas2019armando@gmail.com>
2025-07-02 11:15:25 -0500

    Update order_repository.dart

--- restaurant_bloc_frontend/lib/features/order/domain/repository/order_repository.dart
+++ restaurant_bloc_frontend/lib/features/order/domain/repository/order_repository.dart
@@ -1,5 +1,5 @@
 import 'package:restaurant_bloc_frontend/features/order/domain/entities/order.dart';
 
 abstract class OrderRepository {
-  Future<Order> createOrder(Order order); // Solo entidad de dominio
+  Future<Order> createOrder(Order order);
 }
